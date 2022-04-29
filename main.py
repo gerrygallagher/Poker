937d48c965f0a6e363029e314141665828c3ca7c
Ryan Howard Gwynne <97468950+RyanHGwynne@users.noreply.github.com>
2022-04-29 18:22:46 -0400

    Update main.py

--- main.py
+++ main.py
@@ -6,22 +6,7 @@ def rungame():
     slider.destroy()
     button.destroy()
     label.destroy()
-    root.geometry('1001x651')
-    root.config(bg='#35654d')
-
-    frame00 = Frame(root)
-
-    """image"""
-    # bg = PhotoImage(file="poker_backgrounf.png")
-    # label1 = Label(root, image=bg)
-    # label1.place(x=0, y=0)
-
-
-
-
-
-
-
+    root.geometry('1000x650')
     # initialize and shuffle the deck of cards
     theDeck = Deck()
     theDeck.shuffle()
@@ -37,7 +22,7 @@ def rungame():
     if number_of_players == 2:
         p1 = Label(root, padx=10, pady=10, borderwidth = 3, relief="sunken", text="player 1: "+ str(playerList[0].card1.__str__()) + ' '+ str(playerList[0].card2.__str__()))
         p1.grid(row=0, column=0)
-        p2 = Label(root, padx=10, pady=10, borderwidth = 3, relief="sunken", text="player 2: "+ str(playerList[1].card1.__str__()) + ' '+ str(playerList[1].card2.__str__()))
+        p2 = Label(root, padx=10, pady=10, borderwidth = 3, relief="sunken", text="player 1: "+ str(playerList[1].card1.__str__()) + ' '+ str(playerList[1].card2.__str__()))
         p2. grid(row=1, column=0)
     if number_of_players == 3:
         p1 = Label(root, padx=10, pady=10, borderwidth=3, relief="sunken", text="player 1: " + str(playerList[0].card1.__str__()) + ' ' + str(playerList[0].card2.__str__()))
@@ -111,16 +96,14 @@ def rungame():
         p7.grid(row=6, column=0)
         p8 = Label(root, padx=10, pady=10, borderwidth=3, relief="sunken", text="player 8: " + str(playerList[7].card1.__str__()) + ' ' + str(playerList[7].card2.__str__()))
         p8.grid(row=7, column=0)
-    theBoard = Board(theDeck.deck[(number_of_players * 2) + 1], theDeck.deck[(number_of_players * 2) + 2], theDeck.deck[(number_of_players * 2) + 3], theDeck.deck[(number_of_players * 2) + 4], theDeck.deck[(number_of_players * 2) + 5])
-    board = Label(root, padx=20, pady=20, borderwidth=3, relief='raised', text='BOARD' + '\n' + str(theBoard.flop1) + ' ' + str(theBoard.flop2) + ' ' + str(theBoard.flop3) + ' ' + str(theBoard.turn) + ' ' + str(theBoard.river) )
-    board.grid(row=5, column=4)
-
+        theBoard = Board(theDeck.deck[(number_of_players * 2) + 1], theDeck.deck[(number_of_players * 2) + 2], theDeck.deck[(number_of_players * 2) + 3], theDeck.deck[(number_of_players * 2) + 4], theDeck.deck[(number_of_players * 2) + 5])
+        board = Label(root, padx=20, pady=20, borderwidth=3, relief='raised', text='BOARD' + '\n' + str(theBoard.flop1) + ' ' + str(theBoard.flop2) + ' ' + str(theBoard.flop3) + ' ' + str(theBoard.turn) + ' ' + str(theBoard.river) )
+        board.grid(row=5, column=4)
+#Balls
 
 root = Tk()
 root.resizable(width=False, height=False)
 root.title("Poker")
-
-
 
 button = Button(root, width= 8, height=4, text='Start', command=rungame)
 button.grid(row=0, column=0)
@@ -131,7 +114,11 @@ slider.grid(row=1, column=0)
 label = Label(root, text='How many players are playing?')
 label.grid(row=2, column=0)
 
+bg = PhotoImage(file="poker_background.png")
 
+# Show image using label
+label1 = Label(root, image=bg)
+label1.place(x=0, y=0)
 
 
 
